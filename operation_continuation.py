7aa49266f424591f2ad584b817f2143913490b57
Lyon <lyon@localhost.localdomain>
2018-01-21 10:03:31 +0000

    operation continuation

---
+++ operation_continuation.py
@@ -0,0 +1,20 @@
+a=10
+b=20
+list=(1,2,3,4,5,6)
+if(a in list):
+	print("1-a is available")
+else:
+	print("2-a is not available")
+if(b not in list):
+	print("3-b is not available")
+else:
+	print("4-b is available")
+
+b=4
+if (b in list):
+	print("5-b is available")
+
+else:
+	print("6-b is not available")
+
+print ("goobye")
